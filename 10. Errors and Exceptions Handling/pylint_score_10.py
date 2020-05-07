71976e9ffaa96e35319b07c24db95d45fd178b7c
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-05-07 10:28:13 +0530

    Create pylint_score_10.py
    tested this small code using pylint in cmd
    
    pylint file.py

---
+++ 10. Errors and Exceptions Handling/pylint_score_10.py
@@ -0,0 +1,7 @@
+'''THIS FUNCTION TAKES A SENTENCE AND CAPITALISES IT'''
+def cap_text():
+    '''THIS FUNCTION TAKES A SENTENCE AND CAPITALISES IT'''
+    text = input("Please enter you text:")
+    text2 = text.capitalize()
+    print(text2)
+cap_text()
