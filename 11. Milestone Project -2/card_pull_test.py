774159fedf2d5ca82c5b04a40e39a44bad5f6518
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-05-08 13:24:25 +0530

    Added a python file that pull two random cards

---
+++ 11. Milestone Project -2/card_pull_test.py
@@ -0,0 +1,27 @@
+from random import randint
+class CardDeck():
+    
+    from random import randint
+    top = "----------"
+    bottom = "\n----------"
+    side = "\n|        |"
+    symdict = {1:'♣',2:'♦',3:'♥',4:'♠'}
+    middlel = "\n  "  
+    middler = "  "
+    
+    def __init__(self):
+        pass
+        
+    def card (self):
+        '''
+        This method pulls two random cards.
+        '''
+        x = 1
+        while x < 3:
+            suitn = randint(1,4)
+            number = randint(1,13)
+            card = print(self.top,self.side*2,self.middlel,f'{number}{self.symdict[suitn]}',self.middler,self.side*2,self.bottom)
+            x += 1
+            
+pullcard = CardDeck()
+pullcard.card()
