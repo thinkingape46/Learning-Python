67fb71667e6a3252f8ccd3eb8b953e9ede9eb28e
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-05-24 19:09:33 +0530

    Create file_with_classes.py

---
+++ working_with_classes/file_with_classes.py
@@ -0,0 +1,25 @@
+class Square():
+
+    def __init__(self, num1 = 2):
+
+        self.num1 = num1
+    
+    def squares(self):
+
+        num1_square = square1.num1**2
+
+        print(num1_square)
+
+class Number():
+
+    def __init__(self):
+
+        num1 = 2
+
+        print(num1)
+
+square1 = Square()
+
+number1 = Number()
+
+number1
